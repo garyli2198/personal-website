76e1e6d9f3aceb4cbd2e5a950527fe1af2263fc3
Gary Li <gary.li@stitchfix.com>
2019-07-12 22:54:24 -0700

    fix redirect

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,8 +3,3 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-exports.createPages = ({ graphql, actions }) => {
-  const { createPage, createRedirect } = actions
-
-  createRedirect({ fromPath: "http://iamgary.li/v1", toPath: "https://goofy-fermat-31a7f4.netlify.com", statusCode: 200, isPermanent: true, force: true })
-}
