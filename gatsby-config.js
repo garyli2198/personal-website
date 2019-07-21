9e90452b21a941c350d273201807b60d979cd363
Gary Li <gary.li@stitchfix.com>
2019-07-21 02:10:53 -0700

    add ga

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,6 +43,12 @@ module.exports = {
         display: `fullscreen`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-143390260-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
