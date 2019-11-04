43112baf184d1d9d31587fbc8be2ace9302249a3
Gary Li <garyli2198@gmail.com>
2019-11-04 14:57:01 -0800

    fix widget

--- src/pages/meet.js
+++ src/pages/meet.js
@@ -11,16 +11,28 @@ const Container = styled.div`
   background-color: #161719;
   margin: 0;
 `
+export default class Meet extends React.Component {
+  state = {
+    widget: null
+  }
+  componentDidMount() {
+    this.setState({
+      widget: <CalendlyWidget className="calendly-inline-widget" data-url="https://calendly.com/gary-li/60min?background_color=23262B&text_color=f5f5f5&primary_color=B568FF" />
+    })
+  }
 
-const Meet = () => (
-  <Layout>
-    <Helmet>
-      <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js"></script>
-    </Helmet>
-    <Container>
-      <CalendlyWidget className="calendly-inline-widget" data-url="https://calendly.com/gary-li/60min?background_color=23262B&text_color=f5f5f5&primary_color=B568FF" />
-    </Container>
-  </Layout>
-)
+  render() {
+    const { widget } = this.state;
+    return (
+      <Layout>
+        <Helmet>
+          <script type="text/javascript" src="https://assets.calendly.com/assets/external/widget.js"></script>
+        </Helmet>
+        <Container>
+          {widget}
+        </Container>
+      </Layout>
+    )
+  }
+}
 
-export default Meet;
